0558633f8981813b09ebc3d700cdfebc42ab0747
stephanus88 <stephanus.weber@gmail.com>
2025-06-13 12:47:45 +0200

    4

--- Blatro/UI.java
+++ Blatro/UI.java
@@ -1,35 +1,22 @@
 package Blatro;
-
-
-/**
- * Beschreiben Sie hier die Klasse UI.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class UI
+import sas.*;
+import java.awt.Color;
+public class UI extends Main
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse UI
-     */
+    Text UI_t;
+    Rectangle LeftSide,Mult,Adder,Score;
+    
     public UI()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        
+    UI_t=new Text(500,20,"Pokellatro");
+    LeftSide=new Rectangle(0,100,200,700);
+    LeftSide.setColor(Color.GRAY);
+    
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
+    public void mach()
     {
-        // tragen Sie hier den Code ein
-        return x + y;
+
     }
 }
