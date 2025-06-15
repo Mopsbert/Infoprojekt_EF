e7706870f11198ab47f46b888bf76d6b4e5cfe4a
ElitePro123 <Weili.wang@norbert-gymnasium.de>
2025-06-15 20:27:27 +0200

    Hintergrund Farbanpassung

--- Blatro/Level.java
+++ Blatro/Level.java
@@ -11,18 +11,19 @@ public class Level extends Main
     public Level()
     {
         random = new Random();
-        v.setBackgroundColor(Color.BLUE);
+        v.setBackgroundColor(new Color (136,215,170));
         for( int i = 0; i<100;i++){
             tree(random.nextInt(50,1150),random.nextInt(50,850),random.nextDouble(0.8,1.2));
         }
+    
     }
 
     public void tree(int x, int y, double multiplier)
     {
         Circle c;
         Rectangle r;
-        c = new Circle(x,y,10*multiplier,Color.GREEN);
-        r = new Rectangle(x+8*multiplier,y+20*multiplier,4*multiplier,12*multiplier);
+        c = new Circle(x,y,10*multiplier,new Color (0,128,0));
+        r = new Rectangle(x+8*multiplier,y+20*multiplier,4*multiplier,12*multiplier,new Color (102,51,0));
     }
 
     public void switchscene(int scene)
