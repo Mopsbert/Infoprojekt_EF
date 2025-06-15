d02f2e27d4dd3f5a7736a0fbdc358fa5ab1849a6
ElitePro123 <Weili.wang@norbert-gymnasium.de>
2025-06-15 18:18:52 +0200

    Player zeigt dorthin, wo du hinklickst

--- Blatro/Player.java
+++ Blatro/Player.java
@@ -1,44 +1,47 @@
 package Blatro;
 import sas.*;
-import java.util.Random;
 import java.awt.Color;
 
-public class Player extends Level
-{
+public class Player extends Level {
 
-    
-    Circle c, c1;
-    Ellipse e;
-    Rectangle r, schuss;
-    boolean start;
-    double speed;
-    Sprite figur; 
-    public Player()
-    {
+    Sprite figur;
+    Rectangle mouseArea;
+    double speed = 0.5;
+
+    public Player() {
+        // Unsichtbare Fläche für Mausabfrage
+        mouseArea = new Rectangle(0, 0, 1200, 900, new Color(0, 0, 0, 0));
+
+        // Sprite erstellen und Figur zusammensetzen
         figur = new Sprite();
-        //schuss = new Rectangle (500,500,10,50);
-        r = new Rectangle (500,500,10,50);
 
-        c= new Circle(480,480,20);
-        e= new Ellipse(487,495,25,15,Color.RED);
-        start = false;
-        speed = 0.5;
+        // Bauteile: Rechteck (Körper), Kreis (Kopf), Ellipse (z. B. Visier)
+        Rectangle r = new Rectangle(500, 500, 10, 50);                 // Körper
+        Circle c = new Circle(480, 480, 20);                           // Kopf
+        Ellipse e = new Ellipse(487, 495, 25, 15, Color.RED);         // Visier/Dekoration
+
+        // Teile zum Sprite hinzufügen
         figur.add(r);
         figur.add(c);
-        while(true){
-
+        figur.add(e);
+         
+        while (true) {
             v.wait(1);
-            if(v.keyPressed('a')&&c.getCenterX()>50){ c.move(-speed, 0);e.move(-speed, 0);r.move(-speed, 0);}
-            if(v.keyPressed('d')&&c.getCenterX()<1150){ c.move(+speed, 0);e.move(+speed, 0);r.move(+speed, 0); }
-            if(v.keyPressed('w')&&c.getCenterY()>50){ c.move(0, -speed);e.move(0,-speed);r.move(0, -speed);}
-            if(v.keyPressed('s')&&c.getCenterY()<850){ c.move(0, speed);e.move(0,speed); r.move(0, speed);}
-            if(v.keyPressed('e')){c.turn(1);r.turn(c.getCenterX(),c.getCenterY(),1);e.turn(c.getCenterX(),c.getCenterY(),1);};
-            if(v.keyPressed('q')){c.turn(-1);r.turn(c.getCenterX(),c.getCenterY(),-1);e.turn(c.getCenterX(),c.getCenterY(),-1);}
-            
 
+            // Bewegung mit WASD
+            if (v.keyPressed('a') && figur.getCenterX() > 50)     figur.move(-speed, 0);
+            if (v.keyPressed('d') && figur.getCenterX() < 1150)   figur.move(speed, 0);
+            if (v.keyPressed('w') && figur.getCenterY() > 50)     figur.move(0, -speed);
+            if (v.keyPressed('s') && figur.getCenterY() < 850)    figur.move(0, speed);
+
+            // Maussteuerung: Figur dreht sich zum Klickpunkt
+            if (mouseArea.mouseClicked()) {
+                double mx = mouseArea.getMouseX();
+                double my = mouseArea.getMouseY();
+
+                figur.turnTo(mx, my);
+                figur.turn(-90);
+            }
         }
     }
-
-        
 }
-
